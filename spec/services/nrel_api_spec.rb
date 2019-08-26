238905492e438d658919f554adc6066ddfd063a7
Paul Schlattmann <3031146+pschlatt@users.noreply.github.com>
2019-08-26 10:56:53 -0600

    Final Commit

---
+++ spec/services/nrel_api_spec.rb
@@ -0,0 +1,10 @@
+require "rails_helper"
+
+RSpec.describe "Nrel Api" do
+  it "API_KEY works" do
+    # url = "https://developer.nrel.gov/api/alt-fuel-stations/v1/nearest.json?api_key=#{ENV['API_KEY']}&location=80203"
+    # visit(url)
+    # expect(response).to be_successful
+
+  end
+end
